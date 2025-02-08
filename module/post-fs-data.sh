fb6b76e4dbd317a1eed7cee9d8fcabdb2d984b35
AarifZ <50898218+AarifZ@users.noreply.github.com>
2025-02-08 15:21:55 +0530

    Add files via upload

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,6 +1,17 @@
 MODDIR="${0%/*}"
 MODNAME="${MODDIR##*/}"
 MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
+SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
+
+
+if [ -f "$SUSFS_BIN" ]; then
+stock_path="$(pm path com.google.android.youtube | sed -n '/base/s/package://p')"
+
+[ ! -z "$stock_path" ] && umount -l "$stock_path"
+
+grep com.google.android.youtube /proc/mounts | cut -d " " -f 2 | sed "s/apk.*/apk/" | xargs -r umount -l
+
+else
 
 PROPFILE="$MAGISKTMP/.magisk/modules/$MODNAME/module.prop"
 TMPFILE="$MAGISKTMP/revanced.prop"
@@ -13,3 +24,4 @@ mount --bind "$TMPFILE" "$PROPFILE"
 
 rm -rf "$MODDIR/loaded"
 exit 0
+fi
